fb5ffaad5e3047028b8bb6e59a75033c9ef1e96e
Gery200 <nagygeri4000@gmail.com>
2025-09-05 10:14:06 +0200

    alapok

---
+++ 3.py
@@ -0,0 +1,14 @@
+szám_1 = int(input("Kérem az első számot:"))
+szám_2 = int(input("Kérem a második számot:"))
+
+össz = szám_1 + szám_2
+kiv = szám_1 - szám_2
+szorz = szám_1 * szám_2
+oszt = szám_1 / szám_2
+
+print(f"\\ eredmények \\ \n"
+    f"A két szám összge: {össz} \n"
+      f"a két szám különbsége: {kiv}\n"
+      f"az két szám szorzata: {szorz}\n"
+      f"a két szám hányadosa: {oszt}\n\n"
+      f"\\ vége //")
