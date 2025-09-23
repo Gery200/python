54fa77ccba98ded31fb1082d5446deea31283bd3
Gery200 <nagygeri4000@gmail.com>
2025-09-23 13:12:24 +0200

    listak2

---
+++ listak2.py
@@ -0,0 +1,16 @@
+import random
+
+szamlista = []
+
+#lista feltoltese szamokkal
+
+for x in range (5000):
+    szamlista.append(random.randint(-500, 500)) #hozzáadás
+    
+#lista kiiratasa
+#print(szamlista, sep="")
+
+
+#lista bejarasa
+for x in szamlista:
+    print(x, sep="")
