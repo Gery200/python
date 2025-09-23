8ae6b2bdd2749bc5577bd137716e8fe0a3c0af6c
Gery200 <nagygeri4000@gmail.com>
2025-09-23 12:33:50 +0200

    boolean

---
+++ bool.py
@@ -0,0 +1,13 @@
+a = bool(True)
+b = int(5)
+c = int(12)
+
+e_1 = b > 12
+e2 = b < c 
+e3 = b == c
+e4 = b != c
+print(f"Az e_1 {e_1}")
+print(f"az e2 {e2}")
+
+if e2 == True :
+    print(f"Az állítás igaz")
